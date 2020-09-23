4d5c2364135574863258d33023a25b114e569e71
Amr El-basuony <45123130+Hiden-Evil@users.noreply.github.com>
2020-09-23 12:28:06 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,15 +1,8 @@
 # Landing Page Project
 
-The code in this repository is meant to satisfy the project rubric of the Front Ent Web Developer nanodegree.
-Landing page is the conversion of a static page to a page that allows some sort of interaction. The dynamicity is achieved through javascript.
-
-___
-
-
-Starter code taken from udacity [repository](https://github.com/udacity/fend/tree/refresh-2019/projects/landing-page).
+Landing page is the conversion of a static page to a page that allows some sort of interaction.
+Starter code taken from udacity [repo](https://github.com/udacity/fend/tree/refresh-2019/projects/landing-page).
 
 This project has the scope of converting a static web page in a single interactive page.
-
 It uses javascript to dynamically create navbar links based on the content, when a section is in the viewport it show the active state of that section.
-
 During the scrolling the navbar is automatically highlighting the active section.
--- js/app.js
+++ js/app.js
@@ -1,74 +1,68 @@
-const sections = Array.from(document.getElementsByTagName('section'));
-const title = document.getElementById('title');
-const navbarList = document.getElementById('navbar__list');
+const s = Array.from(document.getElementsByTagName('section'));
+const t = document.getElementById('title');
+const nbl = document.getElementById('navbar__list');
 
-/**
- * jQuery Scroll to element
- * @param target: jQuery selector
- */
-function scrollTo(target) {
-  $('html,body').animate({
-    scrollTop: target ? target.offset().top : 0
-  }, 'slow');
+function jumpTo(target) {
+
+  $('html,body').animate({scrollTop: target ? target.offset().top : 0}, 'fast');
+
 }
 
-/**
- * Function called on each scroll event.
- * It checks for the elements in viewport and sets active classes accordingly.
- */
-function focusOnScroll() {
-  sections.forEach(section => section.classList.remove('section-active'));
-  if (isElementInViewport(title)) {
-    navbarList.childNodes[0].classList.add('navbar-active');
-    navbarList.childNodes.forEach((navLink, index) =>
-      index && navLink.classList.remove('navbar-active'));
-  } else {
-    const activeIndex = sections.findIndex(section => isElementInViewport(section.childNodes[1].childNodes[1]));
-    const activeSection = sections[activeIndex];
-    const activeLink = navbarList.childNodes[activeIndex + 1];
-    activeSection && activeSection.classList.add('section-active');
-    activeLink && activeLink.classList.add('navbar-active');
-    navbarList.childNodes.forEach((navLink, index) =>
-      index !== (activeIndex + 1) && navLink.classList.remove('navbar-active'));
-  }
-}
+function isSectionInView(element) {
 
-window.onscroll = focusOnScroll;
+  let border = element.getBoundingClientRect();
 
-/**
- * Checks if the element is inside the viewport.
- *
- * @param element: HTML element to check
- * @returns {boolean}
- */
-function isElementInViewport(element) {
-  const rect = element.getBoundingClientRect();
   return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    border.top >= 0 &&
+    border.left >= 0 &&
+    border.bottom <= (document.documentElement.clientHeight) &&
+    border.right <= (document.documentElement.clientWidth)
   );
+
 }
 
-/**
- * Populates the navbar list items with the section data.
- */
-function renderNavbar() {
-  const homeLink = document.createElement('li');
-  homeLink.innerText = 'Home';
-  homeLink.className = 'menu__link';
-  homeLink.onclick = () => scrollTo();
-  navbarList.appendChild(homeLink);
-  sections.forEach(section => {
+function highlightOnScroll() {
+
+  if (isSectionInView(t)) {
+
+    nbl.childNodes[0].classList.add('navbar-active');
+    nbl.childNodes.forEach((navLink, index) =>index && navLink.classList.remove('navbar-active'));
+
+  } else if(!isSectionInView(t)){
+
+    let activeSectionIndex = s.findIndex(section => isSectionInView(section.childNodes[1].childNodes[1]));
+    let currentSection = s[activeSectionIndex];
+    let sectionLink = nbl.childNodes[activeSectionIndex + 1];
+
+    currentSection && currentSection.classList.add('section-active');
+    sectionLink && sectionLink.classList.add('navbar-active');
+    nbl.childNodes.forEach((navLink, index) =>index > (activeSectionIndex + 1) && navLink.classList.remove('navbar-active'));
+
+  }
+
+}
+ 
+function drawNavbar() {
+
+  let TopLink = document.createElement('li');
+
+  TopLink.innerText = 'Top';
+  TopLink.className = 'menu__link';
+  TopLink.onclick = () => jumpTo();
+  nbl.appendChild(TopLink);
+  s.forEach(section => {
+
     if (!section.dataset || !section.dataset.nav) return;
-    const item = document.createElement('li');
-    item.innerText = section.dataset.nav;
-    item.className = 'menu__link';
-    item.onclick = () => scrollTo($(`#${section.id}`));
-    navbarList.appendChild(item);
+    let li = document.createElement('li');
+    li.innerText = section.dataset.nav;
+    li.className = 'menu__link';
+    li.onclick = () => jumpTo($(`#${section.id}`));
+    nbl.appendChild(li);
+
   });
+
 }
 
-renderNavbar();
-focusOnScroll();
+window.onscroll = highlightOnScroll;
+drawNavbar();
+highlightOnScroll();
